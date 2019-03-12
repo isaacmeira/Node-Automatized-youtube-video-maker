3474d5e16892bab04ac0124c34d810d365f52d96
isaac meira <fspp@outlook.com>
2019-03-11 23:55:49 -0300

    .

--- index.js
+++ index.js
@@ -1,33 +1,29 @@
-const readline = require ('readline-sync')
-
-
-
-function start () {
-
-    const content = {}
-
-    content.searchTerm = askAndReturnSearchTerm ()
-    content.prefix = askAndReturnPrefix()
-
-    function askAndReturnSearchTerm () {
-        return readline.question ('Type a Wikipedia search term : ')
-    }
-
-
-    function askAndReturnPrefix () {
-
-        const prefixes = ['Who is' , 'What is' , 'The history of'];
-        const selectedPrefixIndex = readline.keyInSelect (prefixes, 'Chose one option : ');
-        const selectedPrefixText = prefixes[selectedPrefixIndex];
-
-        return selectedPrefixText;
-
-    }
-
-
-
-    console.log (content);
-    
+const readline = require('readline-sync')
+const robots = {
+  text: require('./robots/text.js')
 }
 
-start ()
+async function start() {
+  const content = {}
+
+  content.searchTerm = askAndReturnSearchTerm()
+  content.prefix = askAndReturnPrefix()
+
+  await robots.text(content)
+
+  function askAndReturnSearchTerm() {
+    return readline.question('Type a Wikipedia search term: ')
+  }
+
+  function askAndReturnPrefix() {
+    const prefixes = ['Who is', 'What is', 'The history of']
+    const selectedPrefixIndex = readline.keyInSelect(prefixes, 'Choose one option: ')
+    const selectedPrefixText = prefixes[selectedPrefixIndex]
+
+    return selectedPrefixText
+  }
+
+  console.log(content)
+}
+
+start()
